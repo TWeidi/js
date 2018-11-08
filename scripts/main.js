a07cca3f1b8f2f2da09a603fe48e79551043f01c
Thomas Weidinger <thomas.weidinger@rosenberger.com>
2018-11-08 16:31:04 +0100

    Basic UI Layout added

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,29 @@
-function myFunction(x) {
-    console.log('Hallo');
-    x.classList.toggle("change");
+function toggleMenu(obj) {
+    obj.classList.toggle("change");
+
+    nav = document.getElementById('nav');
+    nav.classList.toggle("showNav");
+}
+
+function toggleScreenMode(obj) {
+    let docElm = document.documentElement;
+
+    img = document.getElementById('screenModeImg');
+    str = img.src.toString()
+    
+    if (str.match("src/FullScreenSize.png")) {
+        img.src="src/ReduceScreenSize.png";
+        if (docElm.requestFullscreen) {
+            docElm.requestFullscreen();
+        } else if (docElm.webkitRequestFullScreen) {
+            docElm.webkitRequestFullScreen();
+        }
+    } else {
+        img.src="src/FullScreenSize.png";
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitCancelFullScreen) {
+            document.webkitCancelFullScreen();
+        }
+    }
 }
