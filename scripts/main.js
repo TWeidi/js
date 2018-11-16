e360bfbb8ce04f538dd9c44b1673b56ef12fce9c
Thomas Weidinger <thomas.weidinger@rosenberger.com>
2018-11-16 10:33:43 +0100

    migrated all DOM event handlers to event listeners in the script

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,63 @@
-function toggleMenu(obj) {
-    obj.classList.toggle("change");
 
-    nav = document.getElementById('nav');
-    nav.classList.toggle("showNav");
+window.onload = init;
+
+function init () {
+
+    console.log("Running init func")
+
+    // get DOM elements
+    let menu = document.getElementById('menu');
+    let nav  = document.getElementById('nav');
+
+    // initialize document listeners
+    document.addEventListener('webkitfullscreenchange', () => { toggleScreenBtn() });
+
+    // initialize menu
+    let screenBtn = menu.querySelector('#menuBtnScreen');
+    let navBtn = menu.querySelector('#navBtn');
+
+    screenBtn.addEventListener('click', (e) => { toggleScreenMode(e) });
+    navBtn.addEventListener('click', (e) => { toggleNav(e) });
+
+    // init navigation
+
+
+
+
 }
 
-function toggleScreenMode(obj) {
+function toggleNav(e) {
+
+    let navBtn = document.getElementById('navBtn');
+    let nav = document.getElementById('nav');
+
+    navBtn.classList.toggle("change");
+    nav.classList.toggle("showNav");
+
+}
+
+// toggle display between normal and full screen
+function toggleScreenMode(e) {
+
     let docElm = document.documentElement;
 
-    img = document.getElementById('screenModeImg');
-    str = img.src.toString()
-    
-    if (str.match("src/FullScreenSize.png")) {
-        img.src="src/ReduceScreenSize.png";
-        if (docElm.requestFullscreen) {
-            docElm.requestFullscreen();
-        } else if (docElm.webkitRequestFullScreen) {
-            docElm.webkitRequestFullScreen();
-        }
-    } else {
-        img.src="src/FullScreenSize.png";
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        } else if (document.webkitCancelFullScreen) {
-            document.webkitCancelFullScreen();
-        }
+    if (docElm.requestFullscreen) {
+        docElm.requestFullscreen();
+    } else if (docElm.webkitRequestFullScreen) {
+        docElm.webkitRequestFullScreen();
     }
+
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitCancelFullScreen) {
+        document.webkitCancelFullScreen();
+    }
+}
+
+function toggleScreenBtn() {
+
+    let screenBtn = document.getElementById('menuBtnScreen');
+
+    screenBtn.classList.toggle('menuBtnNormalScreen');
+    screenBtn.classList.toggle('menuBtnFullScreen');
 }
